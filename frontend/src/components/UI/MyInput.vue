da5232a746d11dd64094deab1728c8c11141e59e
Maxim <zhavoronkov03@inbox.ru>
2023-11-17 18:08:16 +0300

    added input component

---
+++ frontend/src/components/UI/MyInput.vue
@@ -0,0 +1,23 @@
+<template>
+    <input :value="modelValue"
+           @input="updateInput" class="form-control" aria-label="Default"
+           aria-describedby="inputGroup-sizing-default" type="text">
+</template>
+
+<script>
+export default {
+    name: "my-input",
+    props:{
+        modelValue: [String, Date, Number, Object]
+    },
+    methods: {
+        updateInput(event) {
+            this.$emit('update:modelValue', event.target.value)
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
